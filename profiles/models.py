f96ae0cb63c58b5f9081fdb37657ea30f2833dc5
Urkel <urkelcodes7@gmail.com>
2024-04-17 17:10:36 +0100

    new commit

--- profiles/models.py
+++ profiles/models.py
@@ -3,24 +3,24 @@ from custom_user.models import User
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=True, blank=True)
-    last_name = models.CharField(max_length=50,  null=True, blank=True)
-    matric_no = models.CharField(max_length=11, null=True, blank=True)
-    avatar = models.ImageField(null=True, default="images/avatar.png", upload_to="images/", blank=True)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50,  null=True)
+    matric_no = models.CharField(max_length=11, null=True)
+    avatar = models.ImageField(null=True, default="images/avatar.png", upload_to="images/")
 
     def __str__(self):
         return str(self.first_name) + " " + str(self.last_name)
 
 class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=150, null=True, blank=True)
+    company_name = models.CharField(max_length=150, null=True)
     bio = models.TextField(blank=True, null=True)
-    phone_number = models.CharField(max_length=11, null=True, blank=True)
-    avatar = models.ImageField(null=True, default="images/avatar.png", upload_to="images/", blank=True)
+    phone_number = models.CharField(max_length=11, null=True)
+    avatar = models.ImageField(null=True, default="images/avatar.png", upload_to="images/")
 
-    address = models.CharField(max_length=300, null=True, blank=True)
-    city = models.CharField(max_length=50, null=True, blank=True)
-    state = models.CharField(max_length=50, null=True, blank=True)
+    address = models.CharField(max_length=300, null=True)
+    city = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
 
 
     class Meta:
--- recruitIT/settings.py
+++ recruitIT/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     # installed apps
     "authenticate",
     "recruitment",
-    "profiles"
+    "profiles",
 ]
 
 # custom user model
