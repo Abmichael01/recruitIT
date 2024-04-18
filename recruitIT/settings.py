848dd666e93bb35620ab1a369429267622a2be07
Urkel <urkelcodes7@gmail.com>
2024-04-18 14:45:01 +0100

    new commit

--- recruitIT/settings.py
+++ recruitIT/settings.py
@@ -110,19 +110,19 @@ WSGI_APPLICATION = 'recruitIT.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DATABSE_NAME"),
-        'USER': os.getenv("DATABSE_USER"),
-        'PASSWORD': os.getenv("DATABSE_PASSWORD"),
-        'HOST': os.getenv("DATABSE_HOST"),
-        'PORT': os.getenv("DATABSE_PORT"),       
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv("DATABSE_NAME"),
+    #     'USER': os.getenv("DATABSE_USER"),
+    #     'PASSWORD': os.getenv("DATABSE_PASSWORD"),
+    #     'HOST': os.getenv("DATABSE_HOST"),
+    #     'PORT': os.getenv("DATABSE_PORT"),       
+    # }
 }
 
 
