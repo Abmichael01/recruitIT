1849ff672a86d50eb1058a98fbb363195ef5ef02
Urkel <urkelcodes7@gmail.com>
2024-04-18 22:46:21 +0100

    new commit

--- recruitIT/settings.py
+++ recruitIT/settings.py
@@ -193,6 +193,7 @@ django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'abmichael109@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
