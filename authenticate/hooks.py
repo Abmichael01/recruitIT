f20b86ebabf9bdc45e4f1ff927014f679a969a75
Urkel <urkelcodes7@gmail.com>
2024-04-20 11:21:14 +0100

    new commit

--- authenticate/hooks.py
+++ authenticate/hooks.py
@@ -1,7 +1,9 @@
 from custom_user.models import User
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def pre_login_user(user, request):
+    messages.info(request, "nice one")
     user = request.user
     if not user.is_active:
         user.is_active = True
