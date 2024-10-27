f6769f676fcc822887956b3d30290440a94d8dd7
Urkel <urkelcodes7@gmail.com>
2024-10-28 00:53:22 +0100

    new commit

--- staticfiles/js/home.js
+++
@@ -1,9 +0,0 @@
-const statesFilterForm = document.querySelector(".filter-form")
-
-
-if (statesFilterForm){
-    const statesFilter = statesFilterForm.querySelector("select")
-    statesFilter.addEventListener("input", ()=>{
-        statesFilterForm.submit()
-    })
-}
