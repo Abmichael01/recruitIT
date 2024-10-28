fe0a2779bfe337ab1bb6f0625cff731a2b544d23
Urkel <urkelcodes7@gmail.com>
2024-10-28 01:23:27 +0100

    new commit

--- recruitIT/urls.py
+++ recruitIT/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path("it-coord/", include("it_coord.urls")),
 
     # google sso path
-    path( "google_sso/", include("django_google_sso.urls", namespace="django_google_sso")),  
+    # path( "google_sso/", include("django_google_sso.urls", namespace="django_google_sso")),  
 ]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
